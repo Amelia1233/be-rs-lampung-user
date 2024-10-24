044ebc24b71fc9dac680385f1a678fca02e87b87
Bedaa1233 <ameliasiregaramel@gmail.com>
2024-10-24 14:28:10 +0700

    true

--- connection/connection.go
+++ connection/connection.go
@@ -13,20 +13,18 @@ import (
 
 func Connection() *gorm.DB {
 	errEnv := godotenv.Load()
-
 	if errEnv != nil {
 		log.Fatal("Failed Load Env")
 	}
 
-	dsn := fmt.Sprintf("user=%s password=%s dbname=%s port=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", 
+    os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
-		log.Fatal("Failed Connect To Database")
+		log.Fatalf("Failed Connect To Database: %v", err)
 	}
 
 	db.AutoMigrate(&entity.Login{})
-
 	return db
 }
